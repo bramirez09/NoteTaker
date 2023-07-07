84b773d97b893c5353b27c5b186375f3c6e36852
brecec <brendaramirez@Brendas-MBP.attlocal.net>
2023-07-06 19:21:44 -0500

    fixed routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fb = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 
@@ -10,26 +9,19 @@ const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-//* `GET *` should return the `index.html` file.
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
-);
-
-//* `GET /notes` should return the `notes.html` file.
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-);
 
 //* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-fb.get('/', (req, res) =>
+app.get('/api/notes', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
-//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
+//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you)
 // POST Route for submitting feedback
-fb.post('/', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
 
@@ -55,8 +47,15 @@ fb.post('/', (req, res) => {
     }
 });
 
-module.exports = fb;
+//* `GET *` should return the `index.html` file.
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
+//* `GET /notes` should return the `notes.html` file.
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
 
 
